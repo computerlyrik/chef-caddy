4add8d7e508eb61b6b9df82e13c310ac17dbfe0e
Christian Fischer <sw-dev@computerlyrik.de>
2015-12-31 03:03:18 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'chef-cookbooks@computerlyrik.de'
 license          'Apache 2.0'
 description      'Installs/Configures caddy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'ark'

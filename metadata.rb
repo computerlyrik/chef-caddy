aff8cfa185184252055fcf57138d8406f2d7d5a4
Christian Fischer <sw-dev@computerlyrik.de>
2016-03-04 20:41:48 +0100

    add issues url

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.4.0'
 
 source_url 'https://github.com/computerlyrik/chef-caddy'
+issues_url 'https://github.com/computerlyrik/chef-caddy/issues'
 
 depends 'ark'

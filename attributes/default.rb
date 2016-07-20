7e7ba7754d373fbd3fc40f990a9562a86b66b482
Daniel Zabel <daniel.zabel@coremedia.com>
2016-07-20 18:05:03 +0200

    configurable binary name to be able to use github releases
    - add binary_name attribute
     - add example to kitchen file

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,6 @@ default['caddy']['hosts'] = {}
 default['caddy']['ulimit'] = 8192
 default['caddy']['ark']['url'] = "https://caddyserver.com/download/build?os=linux&arch=amd64&features=#{node['caddy']['features'].join(',')}"
 default['caddy']['ark']['extension'] = 'tar.gz'
-default['caddy']['ark']['binaries'] = ['./caddy']
+default['caddy']['ark']['binary_name'] = 'caddy'
+default['caddy']['ark']['binaries'] = ["./#{node['caddy']['ark']['binary_name']}"]
 default['caddy']['ark']['strip_components'] = 0

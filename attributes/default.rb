b682869eafd415f03ea94ca187f075150141cea0
Daniel Zabel <daniel.zabel@coremedia.com>
2016-07-20 16:07:28 +0200

    ulimit default value
     - set default value to 8192 to avoid warning
     - fixes #12

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['caddy']['features'] = []
 default['caddy']['email'] = nil
 default['caddy']['hosts'] = {}
-default['caddy']['ulimit'] = 4096
+default['caddy']['ulimit'] = 8192
 default['caddy']['ark']['url'] = "https://caddyserver.com/download/build?os=linux&arch=amd64&features=#{node['caddy']['features'].join(',')}"
 default['caddy']['ark']['extension'] = 'tar.gz'
 default['caddy']['ark']['binaries'] = ['./caddy']
